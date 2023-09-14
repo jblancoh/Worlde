ea7ed5024d0848869058ff60ef1cd28acde36b03
jonathan <zlatanjon@hotmail.com>
2023-09-14 13:05:49 -0600

    Feat(Switch): Improve switch styles

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,27 +1,48 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 const DarkModeSwitch = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
+  const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('theme') === 'dark');
+  
   const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-    if (!isDarkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
+    setIsDarkMode(prevMode => !prevMode);
   };
+  
+  useEffect(() => {
+    if (!isDarkMode) {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  }, [isDarkMode]);
 
   return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          checked={isDarkMode}
-          onChange={toggleDarkMode}
-        />
-      </label>
-    </div>
+    <input
+      className="mr-2 mt-[0.3rem] h-3.5 w-8 
+        appearance-none rounded-lg bg-neutral-300 
+        before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent 
+        before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full 
+        after:border-none after:bg-gray-400  after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] 
+        after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-blue-400 checked:after:absolute 
+        checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 
+        checked:after:rounded-full checked:after:border-none checked:after:bg-primary 
+        checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] 
+        checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] 
+        hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] 
+        focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] 
+        focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full 
+        focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] 
+        checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] 
+        checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-blue-400 dark:after:bg-blue-600 
+        dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] 
+        dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+      type="checkbox"
+      role="switch"
+      checked={isDarkMode}
+      onChange={toggleDarkMode}
+      />
   );
 };
 
